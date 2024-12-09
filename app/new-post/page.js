0554055bcaddf9ed7dd88eb0611f051d05b5aa9f
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-09 17:26:56 +0530

    validating user input through useformstate hook

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,27 +1,46 @@
-import FormSubmit from '@/components/form-submit';
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
+"use client";
+import FormSubmit from "@/components/form-submit";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+import { useFormState } from "react-dom";
 
 export default function NewPostPage() {
   async function createPost(formData) {
     "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
+    let errors = [];
+
+    if (!title && title.trim().length == 0) {
+      errors.push("Title is required.");
+    }
+    if (!content && content.trim().length == 0) {
+      errors.push("Content is required.");
+    }
+
+    if (!image && image.trim().length == 0) {
+      errors.push("Image is required.");
+    }
+
+    if (errors.length) {
+      return [errors];
+    }
 
     storePost({
-      imageUrl: '',
+      imageUrl: "",
       title,
       content,
       userId: 1
-    })
-    redirect('/feed')
+    });
+    redirect("/feed");
   }
 
+  const [state, formAction] = useFormState(createPost, {});
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
@@ -40,7 +59,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-         <FormSubmit />
+          <FormSubmit />
         </p>
       </form>
     </>
