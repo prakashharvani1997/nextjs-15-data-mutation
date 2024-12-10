5e52ac5bfa5f94937bc0f295c006ac68f20318e1
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-10 12:25:29 +0530

    performing optimistic updates with nextJS

--- components/posts.js
+++ components/posts.js
@@ -1,8 +1,10 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
-import { togglePostLikeStatus } from '@/actions/posts';
+"use client";
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { togglePostLikeStatus } from "@/actions/posts";
+import { useOptimistic } from "react";
 
-function Post({ post }) {
+function Post({ post ,action}) {
   return (
     <article className="post">
       <div className="post-image">
@@ -11,38 +13,69 @@ function Post({ post }) {
       <div className="post-content">
         <header>
           <div>
-            <h2>{post.title}</h2>
+            <h2>
+              {post.title}
+            </h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </p>
           </div>
           <div>
-            <form action={togglePostLikeStatus.bind(null,post.id)} className={post.isLiked ? 'liked' :''}>
-            <LikeButton />
+            <form
+              action={action.bind(null, post.id)}
+              className={post.isLiked ? "liked" : ""}
+            >
+              <LikeButton />
             </form>
           </div>
         </header>
-        <p>{post.content}</p>
+        <p>
+          {post.content}
+        </p>
       </div>
     </article>
   );
 }
 
 export default function Posts({ posts }) {
+  const [
+    optimisticPosts,
+    updateOptimisticPosts
+  ] = useOptimistic(posts, (prevPosts, updatedPostId) => {
+    let updatedPostIndex = prevPosts.findIndex(
+      post => post.id == updatedPostId
+    );
+    if (updatedPostIndex == -1) {
+      return prevPosts;
+    }
+    const updatedPost = { ...prevPosts[updatedPostIndex] };
+
+    updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+    updatedPost.isLiked = !updatedPost.isLiked;
+    const newPosts = [...prevPosts];
+
+    newPosts[updatedPostIndex] = updatedPost;
+    return newPosts;
+  });
+
+  async function updatedPost(postId) {
+    updateOptimisticPosts(postId)
+    await togglePostLikeStatus(postId)
+  }
   if (!posts || posts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
   }
 
   return (
     <ul className="posts">
-      {posts.map((post) => (
+      {posts.map(post =>
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatedPost} />
         </li>
-      ))}
+      )}
     </ul>
   );
 }
