a7187c8c7cc3a0282d235eaca6e12ec835bda9a1
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-11 12:03:21 +0530

    working with priority of images

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ export default function Header() {
         <Image src={logo} 
         height={100} // recommended for manual fix height width
         width={100} // recommended for manual fix height width
-        
+        priority
         // sizes='10vW' recommended for responsive design
         alt="Mobile phone with posts feed on it" />
       </Link>
