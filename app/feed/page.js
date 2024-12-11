ae107808f51714c13261e0bcffd8e2603ff4b8a7
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-11 15:15:28 +0530

    Generated dynamic metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,16 +1,14 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
 
+export async function generateMetadata({ queryparams }) {
+  const posts = await getPosts();
 
-// export async function generateMetadata({queryparams}) {
-//   const posts = await getPosts();
-
-// return {
-//   title : 'Latest Posts',
-//   description:'Browse all our Posts'
-// }
-// }
- 
+  return {
+    title: `Browse our ${post.length} posts`,
+    description: "Browse all our Posts"
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
